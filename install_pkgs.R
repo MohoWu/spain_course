58ec8cab5a0e855cfa75d8f2aa824135f127c4a5
Hao Wu <haow@oak.harwell.aeat.com>
2019-10-03 10:22:41 +0100

    add pkg installation code

---
+++ install_pkgs.R
@@ -0,0 +1,8 @@
+# cran packages
+cran_pkgs <- c("tidyverse", "openair", "saqgetr", "worldmet", "devtools")
+
+# install
+install.packages(cran_pkgs)
+
+# install github
+devtools::install_github("mohowu/ricardor")
