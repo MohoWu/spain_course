f95d5cd2f5b81c378044c18b5048f609f5179831
Moho <moho@Mohos-NUC.broadband>
2019-10-03 10:32:59 +0100

    update package installation

--- install_pkgs.R
+++ install_pkgs.R
@@ -1,8 +1,9 @@
 # cran packages
-cran_pkgs <- c("tidyverse", "openair", "saqgetr", "worldmet", "devtools")
+cran_pkgs <- c("tidyverse", "openair", "worldmet", "devtools", "learnr")
 
 # install
 install.packages(cran_pkgs)
 
 # install github
 devtools::install_github("mohowu/ricardor")
+devtools::install_github("skgrange/saqgetr")
